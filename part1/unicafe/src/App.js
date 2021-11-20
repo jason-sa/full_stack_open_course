01265723000532e9ba37d0222e37fb523d15beeb
Jason Salazer-Adams <jsalazer@starbucks.com>
2021-11-20 13:22:23 -0800

    excersize 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -7,6 +7,21 @@ const Display = ( { text, count, addPercentage } ) => (
     {text} {isNaN(count) ? 0 : count} {addPercentage ? '%' : ''}
   </div>
 )
+const Statistics = ( { good, neutral, bad } ) => {
+  const total_clicks = () => good + neutral + bad
+  const scored_clicks = () => good - bad
+
+  return (
+    <>
+      <Display text="good" count={good} />
+      <Display text="neutral" count={neutral} />
+      <Display text="bad" count={bad} />
+      <Display text="all" count={total_clicks()} />
+      <Display text="average" count={scored_clicks() / total_clicks()} />
+      <Display text="positive" count={good / total_clicks() * 100} addPercentage={true} />
+    </>
+  )
+}
 
 const App = () => {
   // save clicks of each button to its own state
@@ -20,8 +35,6 @@ const App = () => {
     if (text === "bad") return setBad(bad+1)
   }
 
-  const total_clicks = () => good + neutral + bad
-  const scored_clicks = () => good - bad
 
   return (
     <div>
@@ -30,12 +43,7 @@ const App = () => {
       <Button handleClick={countClick("neutral")} text="neutral" />
       <Button handleClick={countClick("bad")} text="bad" />
       <Title text="statistics" />
-      <Display text="good" count={good} />
-      <Display text="neutral" count={neutral} />
-      <Display text="bad" count={bad} />
-      <Display text="all" count={total_clicks()} />
-      <Display text="average" count={scored_clicks() / total_clicks()} />
-      <Display text="positive" count={good / total_clicks() * 100} addPercentage={true} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
