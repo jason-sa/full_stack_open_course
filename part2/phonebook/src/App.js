10a0f8aae34a0c2d0c94b34df0e2c7f7dd83497f
Jason Salazer-Adams <jsalazer@starbucks.com>
2021-11-21 15:30:53 -0800

    exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,14 +3,14 @@ import PhoneNumber from './components/PhoneNumber'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      id: 1,
-      number: '040-1234567'
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newSearch, setNewSearch] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -34,10 +34,20 @@ const App = () => {
   const newNameExists = () => persons.filter(person => person.name === newName).length
   const alertNameExists = () => window.alert(`${newName} is already added to phonebook`)
 
+  const filterPhoneBook = (person) => (
+    newSearch === '' ? 
+    true 
+    : person.name.toUpperCase().includes(newSearch.toUpperCase()) 
+  )
+
 
   return (
     <div>
       <h2>Phonebook</h2>
+        <div>
+          filter shown with <input value={newSearch} onChange={handleChange(setNewSearch)} />
+        </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleChange(setNewName)} />
@@ -51,7 +61,11 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => <PhoneNumber key={person.id} {...person} />)}
+        {persons.filter(
+          person => filterPhoneBook(person)
+          ).map(
+            person => <PhoneNumber key={person.id} {...person} />
+            )}
       </ul>
     </div>
   )
