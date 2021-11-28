eeb61237e2c83b3b256140a286294e40249df3da
Jason Salazer-Adams <jsalazer@starbucks.com>
2021-11-27 20:30:45 -0800

    exercise 2.19

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,12 +3,14 @@ import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 import personService from './services/persons'
+import Notification from './components/Notification'
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newSearch, setNewSearch] = useState('')
+  const [successMessage, setSuccessMessage] = useState(null)
 
   useEffect(() => {
     personService.getAll().then(initialPerson => setPersons(initialPerson))
@@ -21,11 +23,24 @@ const App = () => {
     existingPerson.length ? updatePerson({...existingPerson[0], number: newNumber}) : insertPerson()
   }
 
+  const successMessageTimeout = message => {
+    setSuccessMessage(message)
+    setTimeout(() => setSuccessMessage(null), 5000)
+  }
   const updatePerson = updatedPerson => {
     if (alertNameExists()) {
       personService
         .update(updatedPerson.id, updatedPerson)
-        .then(setPersons(persons.map(person => person.id === updatedPerson.id ? updatedPerson : person)))
+        .then(
+          () => {
+            setPersons(persons.map(person => person.id === updatedPerson.id ? updatedPerson : person))
+            successMessageTimeout(`Updated ${updatedPerson.name} number`)
+          }
+        )
+
+      // setSuccessMessage()
+      // console.log(successMessage)
+      // successMessageTimeout()
     }
   }
   const insertPerson = () => {
@@ -34,7 +49,12 @@ const App = () => {
       number: newNumber
     }
 
-    personService.create(newPerson).then(returnedPerson => setPersons(persons.concat(returnedPerson)))
+    personService
+      .create(newPerson)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
+        successMessageTimeout(`Added ${newName}`)
+      })
   }
 
   const removePerson = id => {
@@ -62,6 +82,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+        <Notification message={successMessage} />
         <Filter searchValue={newSearch} onChange={handleChange(setNewSearch)} />
       <h3>add a new</h3>
         <PersonForm onSubmit={addPerson} personInputs={
